d93f8f1fddc96d0b0374571c86ea29bdd6d478ef
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-11-10 21:20:56 +0530

    Calculate Remove Liquidity $ price And Exceed Balance Issue Solved In Add Liquidity And Swap

--- apps/aptos/components/PoolSection/PoolComposition.tsx
+++ apps/aptos/components/PoolSection/PoolComposition.tsx
@@ -1,7 +1,9 @@
+import { formatUSD } from '@sushiswap/format'
 import { List } from '@sushiswap/ui/future/components/list/List'
 import { Icon } from 'components/Icon'
 import { FC } from 'react'
 import { Pool, usePools } from 'utils/usePools'
+import UseStablePrice from 'utils/useStablePrice'
 import { useTokensFromPools } from 'utils/useTokensFromPool'
 import { formatNumber } from 'utils/utilFunctions'
 
@@ -11,13 +13,18 @@ interface PoolCompositionProps {
 
 export const PoolComposition: FC<PoolCompositionProps> = ({ row }) => {
   const { token0, token1 } = useTokensFromPools(row)
+  const token0Price = UseStablePrice(token0)
+  const token1Price = UseStablePrice(token1)
   const balanceX = formatNumber(Number(row?.data?.balance_x?.value), token0.decimals)
   const balanceY = formatNumber(Number(row?.data?.balance_y?.value), token1.decimals)
+  const token0PoolPrice = token0Price ? token0Price * Number(balanceX) : 0
+  const token1PoolPrice = token1Price ? token1Price * Number(balanceY) : 0
+
   return (
     <List>
       <div className="flex items-center justify-between">
         <List.Label>Pool Liquidity</List.Label>
-        <List.Label>{'$00.00m'}</List.Label>
+        <List.Label>{formatUSD(token0PoolPrice + token1PoolPrice)}</List.Label>
       </div>
       <List.Control>
         {
@@ -26,7 +33,7 @@ export const PoolComposition: FC<PoolCompositionProps> = ({ row }) => {
               <div className="flex items-center gap-2">
                 <Icon currency={token0} width={18} height={18} />
                 {Number(balanceX)} {' ' + token0.symbol}
-                <span className="text-gray-600 dark:text-slate-400">({'$00.00m'})</span>
+                <span className="text-gray-600 dark:text-slate-400">({formatUSD(token0PoolPrice)})</span>
               </div>
             </div>
           </List.KeyValue>
@@ -37,7 +44,7 @@ export const PoolComposition: FC<PoolCompositionProps> = ({ row }) => {
               <div className="flex items-center gap-2">
                 <Icon currency={token1} width={18} height={18} />
                 {Number(balanceY)} {' ' + token1.symbol}
-                <span className="text-gray-600 dark:text-slate-400">({'$00.00m'})</span>
+                <span className="text-gray-600 dark:text-slate-400">({formatUSD(token1PoolPrice)})</span>
               </div>
             </div>
           </List.KeyValue>
